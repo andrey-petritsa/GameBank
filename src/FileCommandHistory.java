b027094a384a3bc01f69a3fc37b6200596d1a152
Andrey Petritsa <andrey@petritsa.ru>
2022-10-25 10:59:22 +0300

    Implement push method

--- src/FileCommandHistory.java
+++ src/FileCommandHistory.java
@@ -1,16 +1,34 @@
-import java.io.*;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class FileCommandHistory {
+public class FileCommandHistory implements CommandHistory {
     private final Path pathToFile;
 
     public FileCommandHistory(String path) {
         pathToFile = Paths.get(String.format("%s/history.txt", path));
     }
 
-    public void push(String command) throws IOException {
+    @Override
+    public String get(int i) {
+        try {
+            return Files.readAllLines(Paths.get(pathToFile.toUri())).get(i);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void push(String command) {
+        try {
+            addCommandToFile(command);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void addCommandToFile(String command) throws IOException {
         Files.createDirectories(pathToFile.getParent());
         FileWriter myWriter = new FileWriter(pathToFile.toString(), true);
         myWriter.write(command);
--- test/FileCommandHistoryTest.java
+++ test/FileCommandHistoryTest.java
@@ -1,10 +1,22 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FileCommandHistoryTest {
+    public static final String PATH_TO_FILE = "tmp/history.txt";
+
+    @BeforeEach
+    public void setUp() {
+        File file = new File(PATH_TO_FILE);
+        file.delete();
+    }
+
     @Test
     void push() throws IOException {
         String path = "tmp";
@@ -15,5 +27,17 @@ public class FileCommandHistoryTest {
         BufferedReader br = new BufferedReader(new FileReader(file));
 
         assertEquals("Команда запущена", br.readLine());
+    }
+
+    @Test
+    void get() {
+        String path = "tmp";
+        FileCommandHistory history = new FileCommandHistory(path);
+
+        history.push("Команда 1 запущена");
+        history.push("Команда 2 запущена");
+
+        assertEquals("Команда 1 запущена", history.get(0));
+        assertEquals("Команда 2 запущена", history.get(1));
     }
 }
