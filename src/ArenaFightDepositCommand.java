fe4d448c9535becd97d7c00248024d5f8e98d4ed
Andrey Petritsa <andrey@petritsa.ru>
2022-10-25 16:03:40 +0300

    Implement deposite

--- src/ArenaFightDepositCommand.java
+++ src/ArenaFightDepositCommand.java
@@ -13,6 +13,7 @@ public class ArenaFightDepositCommand implements DepositCommand {
     public void execute() {
         Player player = playerRepository.findById(playerId);
         player.bank.gold += arenaWinStrategy.getReward();
+        playerRepository.save(player);
         String commandMessage = String.format("Команда положить %s золота в банк (сражаясь на арене). От игрока: %s. В клан: %s. Золота итого: %s", arenaWinStrategy.getReward(), player.id, player.bank.id, player.bank.gold);
         this.commandHistory.push(commandMessage, this);
     }
--- test/ArenaFightDepositCommandTest.java
+++ test/ArenaFightDepositCommandTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ArenaFightDepositCommandTest {
     @Test
-    void execute() {
+    void history() {
         Bank bank = new Bank(1, 100);
         Player player = new Player(1, bank);
         InMemoryPlayerRepository stubPlayerRepository = new InMemoryPlayerRepository();
@@ -16,4 +16,18 @@ public class ArenaFightDepositCommandTest {
 
         assertEquals("Команда положить 1000 золота в банк (сражаясь на арене). От игрока: 1. В клан: 1. Золота итого: 1100", history.get(0));
     }
+
+    @Test
+    void deposit() {
+        int bankGold = 100;
+        Bank bank = new Bank(1, bankGold);
+        Player player = new Player(1, bank);
+        InMemoryPlayerRepository mockPlayerRepository = new InMemoryPlayerRepository(player);
+        ArenaFightDepositCommand command = new ArenaFightDepositCommand(new InMemoryCommandHistory(), mockPlayerRepository, new GreatArenaWinStrategy());
+
+        command.execute();
+
+        assertEquals(1100, mockPlayerRepository.findById(1).bank.gold);
+    }
 }
+
